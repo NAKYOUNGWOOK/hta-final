4dc9cd5cdfdfda8791b7cdc69a7a130dc2fcc093
DESKTOP-22RBN94\Administrator <hyadain12002@gmail.com>
2023-03-14 14:48:11 +0900

    nkw commit

---
+++ nkw/src/main/java/com/groupware/NkwApplication.java
@@ -0,0 +1,13 @@
+package com.groupware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NkwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NkwApplication.class, args);
+	}
+
+}
