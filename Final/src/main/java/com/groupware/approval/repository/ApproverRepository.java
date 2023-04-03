14c80055a87b93e36459c12e5e6ba36cda48c8e9
ChoiJongKwan <57290947+ChoiJongKwan@users.noreply.github.com>
2023-04-03 09:25:39 +0900

    Delete Final directory

--- Final/src/main/java/com/groupware/approval/repository/ApproverRepository.java
+++
@@ -1,9 +0,0 @@
-package com.groupware.approval.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.groupware.approval.entity.Approver;
-
-public interface ApproverRepository extends JpaRepository<Approver, Long> {
-
-}
--- Final/src/main/java/com/groupware/approval/repository/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.groupware.approval.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.groupware.approval.entity.Document;
-
-public interface DocumentRepository extends JpaRepository<Document, Long> {
-
-}
